0df1219538f8436f93c9d7873ecf4c9fb490c1d4
theo-viard <theoviard2701@gmail.com>
2024-10-09 15:50:59 +0200

    Ajout des fonctions de base et d'une question test

--- gestion_questions.py
+++ gestion_questions.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 class Question:
     def __init__(self, idQuest,  question, answers, category, difficulty):
@@ -7,6 +8,24 @@ class Question:
         self.answers = answers
         self.category = category
         self.difficulty = difficulty
+    
+    def getanswers(self):
+        answers = []
+        for answer in self.answers:
+            if answer["isCorrect"]:
+                answers.append(answer)
+        for i in range(3):
+            random_answer = random.choice(self.answers)
+            if random_answer not in answers:
+                answers.append(random_answer)
+        random.shuffle(answers)
+        return answers
+    
+    def check_answer(self, answer):
+        for ans in self.answers:
+            if ans["text"] == answer:
+                return ans["isCorrect"]
+        return False
 
     def __str__(self):
         return f"{self.question} - {self.answers} - {self.category} - {self.difficulty}"
@@ -36,10 +55,25 @@ def get_questions_by_category(category):
 
 def get_questions_by_difficulty(difficulty):
     questions = get_questions()
-    questions_by_difficulty = []
     for question in questions:
-        if question.difficulty == difficulty:
-            questions_by_difficulty.append(question)
-    return questions_by_difficulty
+        print(question.question)
 
-print(get_questions())
+    
+def get_random_question(questions):
+    return random.choice(questions)
+
+def main():
+    questions = get_questions()
+    for question in questions:
+        print(question.question)
+        reponses = question.getanswers()
+        for answer in reponses:
+            print(f"{reponses.index(answer)+1}. {answer['text']}")
+        choice = input("Entre ta réponse: ")
+        if question.check_answer(reponses[int(choice)-1]):
+            print("Correct")
+        else:
+            print("Incorrect")
+
+if __name__ == "__main__":
+    main()
